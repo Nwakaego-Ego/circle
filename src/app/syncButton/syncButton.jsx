e17b5d734d6c0413c0c785048f6a0b7c75ce8cd8
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-01-20 14:33:05 -0800

    sync buton

--- src/app/syncButton/syncButton.jsx
+++ src/app/syncButton/syncButton.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Modal.css"; // Import your modal styles
 
-export default function syncButton() {
-  return <div>syncButton</div>;
-}
+const Modal = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    // Additional logic to clear modal content can be added here
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <p>Modal Content</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Modal;
