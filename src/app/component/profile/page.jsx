0f5999a8561a78dbb6500429dd146dd6cb267795
Ego Nwaekpe <glorynwaekpe@gmail.com>
2024-02-23 23:47:48 +0100

    i added edit in profile

--- src/app/component/profile/page.jsx
+++ src/app/component/profile/page.jsx
@@ -1,3 +1,119 @@
+// "use client";
+// import React, { useState } from "react";
+// import Image from "next/image";
+// import { imgCollection } from "../../imgCollection";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faImages,
+//   faBookmark,
+//   faUserLock,
+//   faEdit,
+//   faEyeSlash,
+//   faEye,
+// } from "@fortawesome/free-solid-svg-icons";
+
+// import "./profile.css";
+
+// const Profile = () => {
+//   console.log(imgCollection);
+//   const [images, setImages] = useState([]);
+//   const [randomCode, setRandomCode] = useState();
+//   const [codeHidden, setCodeHidden] = useState(false);
+
+//   const handleImgCollection = () => {
+//     if (images.length > 0) {
+//       setImages([]);
+//     } else {
+//       setImages(imgCollection);
+//     }
+//   };
+
+//   console.log(images);
+
+//   const generateRandomCode = () => {
+//     // const code = generateRandomCode();
+//     setRandomCode(Math.floor(Math.random() * 90000) + 10000);
+//     // setRandomCode(code);
+//   };
+
+//   const HandleFriendshipCode = () => {
+//     setRandomCode("");
+//     setCodeHidden(!codeHidden);
+//   };
+
+//   return (
+//     <>
+//       <div className="container-wrapper">
+//         <div className="profile-container">
+//           <div className="profile-main">
+//             <Image
+//               src="/nwakaego.jpeg"
+//               alt="Note Image"
+//               width={300}
+//               height={200}
+//               layout="fit"
+//               objectFit="cover"
+//               className="rounded-full w-60 h-60 mb-8"
+//             />
+//             <FontAwesomeIcon icon={faEdit} className="profile-edit-button" />
+//             <p className="profile-profile">
+//               Lorem ipsum dolor sit amet consectetur adipisicing elit.
+//               Asperiores ullam, mollitia similique quas, totam, quia aba
+//               excepturi repellat nihil consectetur possimus quae a corporis id
+//               eius cumque ducimus maiores quidem.
+//             </p>
+//             <div className="flex profile-buttons ">
+//               <button onClick={handleImgCollection}>
+//                 <FontAwesomeIcon
+//                   icon={faImages}
+//                   className="profile-memory profile-icon"
+//                 />
+//               </button>
+//               <FontAwesomeIcon
+//                 icon={faBookmark}
+//                 className="profile-bookmark profile-icon"
+//               />
+//               <button onClick={generateRandomCode}>
+//                 <FontAwesomeIcon
+//                   icon={faUserLock}
+//                   className="profile-request-code profile-icon"
+//                 />
+//               </button>
+//             </div>
+//             {images.map((item, id) => {
+//               return (
+//                 <div key={id}>
+//                   <img
+//                     src={item.image}
+//                     alt="profile_img"
+//                     width={300}
+//                     height={200}
+//                     layout="fit"
+//                     objectFit="cover"
+//                     className="rounded-full w-60 h-60 mb-8"
+//                   />
+//                 </div>
+//               );
+//             })}
+//             <div className="flex">
+//               {randomCode}
+//               <button onClick={HandleFriendshipCode}>
+//                 {codeHidden ? (
+//                   <FontAwesomeIcon icon={faEyeSlash} className="" />
+//                 ) : (
+//                   <FontAwesomeIcon icon={faEyeSlash} />
+//                 )}
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Profile;
+
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
@@ -9,14 +125,19 @@ import {
   faUserLock,
   faEdit,
   faEyeSlash,
+  faEye,
 } from "@fortawesome/free-solid-svg-icons";
 
 import "./profile.css";
 
 const Profile = () => {
-  console.log(imgCollection);
   const [images, setImages] = useState([]);
-  const [randomCode, setRandomCode] = useState();
+  const [randomCode, setRandomCode] = useState("");
+  const [codeHidden, setCodeHidden] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [profileText, setProfileText] = useState(
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ullam, mollitia similique quas, totam, quia ab excepturi repellat nihil consectetur possimus quae a corporis id eius cumque ducimus maiores quidem."
+  );
 
   const handleImgCollection = () => {
     if (images.length > 0) {
@@ -26,16 +147,25 @@ const Profile = () => {
     }
   };
 
-  console.log(images);
-
-  const generateRandomCode = () => {
-    // const code = generateRandomCode();
-    setRandomCode(Math.floor(Math.random() * 90000) + 10000);
-    // setRandomCode(code);
+  const HandleFriendshipCode = () => {
+    setRandomCode("");
+    setCodeHidden(!codeHidden);
   };
 
-  const hideCode = () => {
-    setRandomCode("");
+  const generateRandomCode = () => {
+    setRandomCode(Math.floor(Math.random() * 90000) + 10000);
+  };
+
+  const handleProfileEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleProfileSave = () => {
+    setIsEditing(false);
+  };
+
+  const handleProfileTextChange = (e) => {
+    setProfileText(e.target.value);
   };
 
   return (
@@ -52,13 +182,19 @@ const Profile = () => {
               objectFit="cover"
               className="rounded-full w-60 h-60 mb-8"
             />
-            <FontAwesomeIcon icon={faEdit} className="profile-edit-button" />
-            <p className="profile-profile">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Asperiores ullam, mollitia similique quas, totam, quia ab
-              excepturi repellat nihil consectetur possimus quae a corporis id
-              eius cumque ducimus maiores quidem.
-            </p>
+            <FontAwesomeIcon
+              icon={faEdit}
+              className="profile-edit-button"
+              onClick={handleProfileEdit}
+            />
+            {isEditing ? (
+              <textarea
+                value={profileText}
+                onChange={handleProfileTextChange}
+              />
+            ) : (
+              <p className="profile-profile">{profileText}</p>
+            )}
             <div className="flex profile-buttons ">
               <button onClick={handleImgCollection}>
                 <FontAwesomeIcon
@@ -77,27 +213,30 @@ const Profile = () => {
                 />
               </button>
             </div>
-            {images.map((item, id) => {
-              return (
-                <div key={id}>
-                  <img
-                    src={item.image}
-                    alt="profile_img"
-                    width={300}
-                    height={200}
-                    layout="fit"
-                    objectFit="cover"
-                    className="rounded-full w-60 h-60 mb-8"
-                  />
-                </div>
-              );
-            })}
+            {images.map((item, id) => (
+              <div key={id}>
+                <img
+                  src={item.image}
+                  alt="profile_img"
+                  width={300}
+                  height={200}
+                  layout="fit"
+                  objectFit="cover"
+                  className="rounded-full w-60 h-60 mb-8"
+                />
+              </div>
+            ))}
             <div className="flex">
               {randomCode}
-              <button onClick={hideCode}>
-                <FontAwesomeIcon icon={faEyeSlash} className="" />
+              <button onClick={HandleFriendshipCode}>
+                {codeHidden ? (
+                  <FontAwesomeIcon icon={faEyeSlash} className="" />
+                ) : (
+                  <FontAwesomeIcon icon={faEyeSlash} />
+                )}
               </button>
             </div>
+            {isEditing && <button onClick={handleProfileSave}>Save</button>}
           </div>
         </div>
       </div>
